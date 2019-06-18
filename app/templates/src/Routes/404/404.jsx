c2a570f0056c42c8fd32421fece395f2432fee38
mohammedgqudah <mohghq@gmail.com>
2019-06-18 12:27:06 +0300

    Kawaii 404 page 🎀

--- app/templates/src/Routes/404/404.jsx
+++ app/templates/src/Routes/404/404.jsx
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
-import './404.scss';
+import { Ghost } from "react-kawaii";
+
+import "./404.scss";
+
 class ErrorPage extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-		this.state = {
-
-		};
-	}
-
-	render() {
-		// TODO: React-Kawaii
-		return (
-			<div className="page404">
-				
-			</div>
-		);
-	}
+  render() {
+    return <div className="page404" >
+		<Ghost mood="sad" color="#9bace4" size={250} className="ghost"/>
+		<h2>404 Page Not Found</h2>
+	</div>;
+  }
 }
 
 export default ErrorPage;
